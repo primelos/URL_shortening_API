7f65fac0936d0175384216214c46f42fef160964
Ale <alearanibar@gmail.com>
2022-04-27 20:58:33 -0700

    intro body and search built out

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
-const Button = () => {
-  return <div></div>;
+const Button = (props) => {
+  return <ButtonContainer>{props.text}</ButtonContainer>;
 };
 
 export default Button;
+
+const ButtonContainer = styled.button`
+  border: none;
+  border-top-right-radius: 25px;
+  border-top-left-radius: 25px;
+  border-bottom-right-radius: 25px;
+  border-bottom-left-radius: 25px;
+  padding: 8px 20px;
+  background-color: #58d1d1;
+  font-size: 15px;
+  color: #fff;
+  font-weight: bold;
+  font-family: "Poppins", sans-serif;
+  &:hover {
+    cursor: pointer;
+    background-color: #9ce2e2;
+  }
+`;
